b20f099151de27687de23cbc760593aa47fe41ef
iuryvieira <iuryvieira.lippo@gmail.com>
2019-04-12 19:56:03 -0300

    add widget column with child text

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MaterialApp(
-    title: "Person Counter",
-    home: Container(color: Colors.white,),
-  ));
+      title: "Person Counter",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+         
+        ],
+      )));
 }
