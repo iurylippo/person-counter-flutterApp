83b24880be383c221f03077d4909bb97810bd8bf
iuryvieira <iuryvieira.lippo@gmail.com>
2019-04-12 21:44:12 -0300

    add button function increment and decrecent

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Person Counter",
-      home: Home()));
+  runApp(MaterialApp(title: "Person Counter", home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -12,55 +10,90 @@ class Home extends StatefulWidget {
 }
 
 class _State extends State<Home> {
+  int _people = 0;
+  String _infoText = "Pode entrar!";
+  
+
+  void _changePeople(int delta) {
+    setState(() {
+      if (_people + delta >= 0 && _people + delta <= 15) {
+        _people += delta;
+        if ( _people == 1) {
+         
+          _infoText = "Tem $_people pessoa";
+        } 
+        else if (_people == 0 && delta < 1) {
+          _infoText = "Não tem ninguém";
+        }
+
+        else if (_people == 15) {
+          _infoText = "Está lotado!";
+        }
+
+        else {
+          _infoText = "Tem $_people pessoas";
+          }
+
+      } else if(_people + delta > 15) {
+        _infoText = "Não pode entrar mais que $_people";
+      } else {
+        _infoText = "Pra que menos que isso? XD";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
-        children: <Widget>[
-          Image.asset("images/restaurant.jpg",
-              fit: BoxFit.cover, height: 1000.0),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Pessoas: 0",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
+      children: <Widget>[
+        Image.asset("images/restaurant.jpg", fit: BoxFit.cover, height: 1000.0),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_people",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
                   ),
-                ],
-              ),
-              Text(
-                "Pode Entrar!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 30.0),
-              )
-            ],
-          )
-        ],
-      );
+                ),
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
